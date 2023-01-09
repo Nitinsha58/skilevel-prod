bb979ea9cdc5b3f905c46eec5d987f4faa20e88b
Nitin <nitin8285824551@gmail.com>
2023-01-09 14:53:15 +0530

    add skilevel design and content

---
+++ app.js
@@ -0,0 +1,27 @@
+const countdown = ()=>{
+    const countDate = new Date('Jan 30, 2023, 10:26:00').getTime()
+    const now = new Date().getTime();
+
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap/day);
+    const textHour = Math.floor((gap % day) / hour)
+    const textMinute = Math.floor((gap % hour) / minute)
+    const textSecond = Math.floor((gap % minute) / second)
+
+    document.getElementById("day").innerText = textDay
+    document.getElementById('hour').innerText = textHour
+    document.getElementById('minute').innerText = textMinute
+    document.getElementById('second').innerText = textSecond
+}
+
+countdown();
+
+setInterval(() => {
+    countdown();
+}, 1000);
